09c1b717b9c916baa83e982876e53c5d0c9ddefa
Leonardo Vilches <leonardo.vilches@vegasolutions.com.br>
2021-04-12 11:12:12 -0300

    Validações campos do cadastro de anuncio

--- lib/screens/create/components/cep_field.dart
+++ lib/screens/create/components/cep_field.dart
@@ -3,31 +3,38 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:sos_services/stores/cep_store.dart';
+import 'package:sos_services/stores/create_store.dart';
 
 class CepField extends StatelessWidget {
-  final CepStore cepStore = CepStore();
+  CepField(this.createStore) : cepStore = createStore.cepStore;
+
+  final CreateStore createStore;
+  final CepStore cepStore;
   @override
   Widget build(BuildContext context) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
-        TextFormField(
-          onChanged: cepStore.setCep,
-          keyboardType: TextInputType.number,
-          inputFormatters: [
-            FilteringTextInputFormatter.digitsOnly,
-            CepInputFormatter(),
-          ],
-          decoration: InputDecoration(
-            labelText: 'CEP *',
-            labelStyle: TextStyle(
-              fontWeight: FontWeight.w800,
-              color: Colors.grey,
-              fontSize: 18,
+        Observer(builder: (_) {
+          return TextFormField(
+            onChanged: cepStore.setCep,
+            keyboardType: TextInputType.number,
+            inputFormatters: [
+              FilteringTextInputFormatter.digitsOnly,
+              CepInputFormatter(),
+            ],
+            decoration: InputDecoration(
+              labelText: 'CEP *',
+              errorText: createStore.addressError,
+              labelStyle: TextStyle(
+                fontWeight: FontWeight.w800,
+                color: Colors.grey,
+                fontSize: 18,
+              ),
+              contentPadding: const EdgeInsets.fromLTRB(16, 10, 12, 10),
             ),
-            contentPadding: const EdgeInsets.fromLTRB(16, 10, 12, 10),
-          ),
-        ),
+          );
+        }),
         Observer(
           builder: (_) {
             if (cepStore.address == null &&
